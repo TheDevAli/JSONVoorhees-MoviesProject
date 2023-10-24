f3122a19d5c893852179814f0c5d72052b2eb6e2
David Kelly <davek032@outlook.com>
2023-10-24 11:16:34 +0100

    6 Add Service Layer (#13)
    * #6 Unified Service layer created. Borrowed existing code from web branch
    
    * #6 Unified Service layer fleshed out with basic methods

---
+++ springapi/src/main/java/com/sparta/jsonvoorhees/springapi/service/ServiceLayer.java
@@ -0,0 +1,91 @@
+package com.sparta.jsonvoorhees.springapi.service;
+
+import com.sparta.jsonvoorhees.springapi.model.entities.*;
+import com.sparta.jsonvoorhees.springapi.model.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServiceLayer {
+    private final CommentRepository commentRepository;
+    private final MovieRepository movieRepository;
+    private final TheaterRepository theaterRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ServiceLayer(CommentRepository commentRepository,
+                             MovieRepository movieRepository,
+                             ScheduleRepository scheduleRepository,
+                             TheaterRepository theaterRepository,
+                             UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.movieRepository = movieRepository;
+        this.scheduleRepository = scheduleRepository;
+        this.theaterRepository = theaterRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Movie> getAllMoviesWithTitle(String title) {
+        List<Movie> fullMovieList = movieRepository.findAll();
+        if (title == null) {
+            return movieRepository.findAll();
+        }
+        else {
+            List<Movie> filteredMovies = new ArrayList<>();
+            for (Movie movie : fullMovieList) {
+                if (movie.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                    filteredMovies.add(movie);
+                }
+            }
+            return filteredMovies;
+        }
+    }
+
+    public List<Schedule> getSchedulesForTheaters(String theaterId)
+    {
+        return null;
+    }
+
+    public List<Comment> getCommentsByMovie(String movieId)
+    {
+        return null;
+    }
+
+    public List<Comment> getCommentsByUser(String userId)
+    {
+        return null;
+    }
+
+    public List<Theater> getAllTheaters()
+    {
+        return null;
+    }
+
+    //region Basic Getters
+    public Optional<Movie> getMovieById(String movieId)
+    {
+        return null;
+    }
+
+    public Optional<Theater> getTheaterById(String theaterId)
+    {
+        return null;
+    }
+
+    public Optional<Schedule> getScheduleById(String scheduleId)
+    {
+        return null;
+    }
+
+    public Optional<User> getUserById(String userId)
+    {
+        return null;
+    }
+    //endregion
+}
